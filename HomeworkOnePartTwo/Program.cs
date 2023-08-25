d806ecc43794f16adcdf83cbec002cb32d33b503
W7295232 <W7295232@STU-JAVA-IC-2>
2023-08-25 17:01:42 +0200

    Add project files.

---
+++ HomeworkOnePartTwo/Program.cs
@@ -0,0 +1,108 @@
+﻿//Chapter No. 1 Exercise no. 2
+//File Name: HomeworkOnePartTwo.cs
+//@author: Nasi Bode
+//Date: August 25 2023
+//
+//Problem Statement: Write a program that displays a simple picture of your choice. It could be
+// a car, a house, a flower, a tree, a square, a 3-D box, a star or other shapes. Print your
+// results to the screen. 
+
+//Overall Plan:
+// 1. print welcome message to the screen
+// 2. use Console.WriteLine to display a picture using asteriks and dashes
+// 3. Ensure picture is displayed to screen
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeworkOnePartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! This program will display the Statue of Liberty:");
+            Console.WriteLine("_1¶¶¶¶¶¶1____________________________________");
+            Console.WriteLine("8¶8__¶¶1_____________________________________");
+            Console.WriteLine("1¶¶¶¶¶¶______________________________________");
+            Console.WriteLine("8¶¶¶¶¶¶______________________________________");
+            Console.WriteLine("¶¶¶88¶¶1_____________________________________");
+            Console.WriteLine("_¶¶¶¶81______________________________________");
+            Console.WriteLine("_1__¶¶_______________________________________");
+            Console.WriteLine("_8_1¶¶¶______________________________________");
+            Console.WriteLine("__8¶¶¶8______________________________________");
+            Console.WriteLine("___¶¶¶¶______________________________________");
+            Console.WriteLine("___1_¶¶______________________________________");
+            Console.WriteLine("___8__¶¶__________81_________________________");
+            Console.WriteLine("___8__8¶__________¶¶_____88__________________");
+            Console.WriteLine("___¶___¶8__1¶8____8¶____¶¶___________________");
+            Console.WriteLine("___¶___¶¶____¶¶8___¶1__¶¶____181_____________");
+            Console.WriteLine("___¶___¶¶______¶¶¶¶¶¶¶¶¶1_1¶¶8_______________");
+            Console.WriteLine("___18__¶¶1__11_8¶¶¶¶¶¶¶¶¶¶¶1_________________");
+            Console.WriteLine("____8__8¶¶¶88¶¶¶¶¶¶118¶¶¶¶8__111_____________");
+            Console.WriteLine("____8__18¶¶___¶¶¶¶¶18¶¶¶¶¶¶¶¶¶811____________");
+            Console.WriteLine("___8¶_8¶_1¶¶8¶¶¶8___¶__¶¶¶8__________________");
+            Console.WriteLine("__1¶¶181_1¶¶__8¶___8¶¶¶¶¶¶8__________________");
+            Console.WriteLine("__¶¶8_81188¶¶1_¶8__88¶¶¶¶¶¶__________________");
+            Console.WriteLine("__¶8_18¶¶¶_1¶¶¶88¶¶¶¶¶¶¶__¶1_________________");
+            Console.WriteLine("__¶8_8¶¶1881¶¶¶¶¶8__8¶¶¶1__8_________________");
+            Console.WriteLine("___1¶¶8888818¶¶¶¶¶____¶¶¶__8_________________");
+            Console.WriteLine("___111_88181_8¶¶¶¶¶¶¶888¶¶8¶_________________");
+            Console.WriteLine("___81__1881_8___¶¶¶1__8888_11________________");
+            Console.WriteLine("____88_881____1¶¶¶__188_8¶11_81______________");
+            Console.WriteLine("_____¶1¶8__8¶¶¶¶1_1_1¶8_¶_1¶¶8¶______________");
+            Console.WriteLine("_____¶181_8¶¶¶__8¶_1_¶1_¶1_¶¶¶¶1_______111___");
+            Console.WriteLine("_____811¶118__8¶8_8_¶¶_8¶__¶¶¶¶8_____1¶111__¶");
+            Console.WriteLine("_____8_¶8_1_8¶¶1_¶_1¶_181¶¶88¶¶81___81_____¶¶");
+            Console.WriteLine("_____81__8¶¶818_¶__¶_¶_81¶¶¶1¶¶8¶1_81_¶¶1_¶¶8");
+            Console.WriteLine("_____¶___¶81_8_¶8_¶_¶8_¶__¶¶¶¶8¶¶¶8__8___¶¶8_");
+            Console.WriteLine("____811_8_1_8_¶¶_¶_1¶__¶¶¶¶8¶¶8¶811_11_188¶8_");
+            Console.WriteLine("____8_8811_¶_1¶_81_¶118¶¶81¶¶¶¶8¶¶¶11_8¶¶¶¶1_");
+            Console.WriteLine("___1¶_88__¶8_¶8_8__¶¶81¶8__¶_¶¶¶¶¶¶1_8¶¶8¶¶__");
+            Console.WriteLine("___1¶_¶118¶_¶¶_¶___¶8__8___¶_8¶¶8¶___¶¶88¶1__");
+            Console.WriteLine("____¶_111¶__¶_1__¶__1__¶1__¶¶¶11¶1_1¶¶¶8¶1___");
+            Console.WriteLine("____¶¶18¶¶_¶¶_8_¶¶_11__¶1__¶¶___¶_1¶¶1¶¶_____");
+            Console.WriteLine("____8111¶_¶¶_1_8¶__11__¶8__¶¶___¶¶¶¶¶¶¶¶_____");
+            Console.WriteLine("____11_¶8_¶8_8_¶___8___¶8__¶¶___¶¶¶¶¶¶¶¶_____");
+            Console.WriteLine("____818¶_¶¶_1_¶¶__¶¶___¶8__¶¶___¶¶¶¶¶¶¶8_____");
+            Console.WriteLine("____88¶_1¶____¶__888___¶1__¶¶___1¶81¶¶¶______");
+            Console.WriteLine("____¶¶1_¶¶___1_1_______¶1__¶¶____18__¶8______");
+            Console.WriteLine("____¶¶__¶_____1¶_______¶1__¶¶___11¶¶1________");
+            Console.WriteLine("_____¶_¶¶____1¶1_______¶___¶¶1__¶11__________");
+            Console.WriteLine("_____¶¶¶___8¶¶1___8____¶___¶¶¶8_8____________");
+            Console.WriteLine("_____1¶8__88¶8___8¶___8¶___¶¶___¶____________");
+            Console.WriteLine("_____8¶___118____¶1___8¶___¶¶___¶____________");
+            Console.WriteLine("_____81__1¶11____¶___1¶___1¶¶___¶____________");
+            Console.WriteLine("_____8___¶1______¶___¶1__¶8__1__¶____________"); 
+            Console.WriteLine("_____81__1_______1_____¶8____1__¶____________");
+            Console.WriteLine("_____¶8____________8¶8¶88_18_188¶____________");
+            Console.WriteLine("_____¶¶__________8¶¶¶18¶1¶¶¶¶8¶¶¶____________");
+            Console.WriteLine("_____¶¶___1______¶¶1_8_¶1¶¶__¶__¶1___________");
+            Console.WriteLine("_____88__¶1______8¶__¶_¶881___11¶____________");
+            Console.WriteLine("_____81__8_________¶¶¶_¶81111_8¶1____________");
+            Console.WriteLine("_____8__8__________8¶¶_¶8_118_8¶_____________");
+            Console.WriteLine("_____8_18_____1118881¶_¶1_118_81_____________");
+            Console.WriteLine("_____811____¶¶¶8¶8_11¶_¶8111¶_8______________");
+            Console.WriteLine("_____88___1¶88__8__18¶_¶818_8_81_____________");
+            Console.WriteLine("_____88__1¶_1¶_188__¶¶_¶818_8_1______________");
+            Console.WriteLine("_____81__8__¶8_¶¶¶__¶¶_¶¶81_¶¶¶______________");
+            Console.WriteLine("_____18___18¶11818__¶¶_¶¶81_8¶¶______________");
+            Console.WriteLine("______¶__188118__8_1¶¶_¶¶81__¶1______________");
+            Console.WriteLine("______111¶8111¶_81__¶¶_¶¶81_1¶¶______________");
+            Console.WriteLine("_______¶8¶11811_¶88_¶¶_¶¶1__1¶1______________");
+            Console.WriteLine("______881¶181___8818¶¶_¶¶_1_181______________");
+            Console.WriteLine("_____81__8_¶1_1¶_818¶¶_¶¶___1_81_____________");
+            Console.WriteLine("___1¶___1_111_¶¶__1_¶¶_¶8_1_1118_____________");
+            Console.WriteLine("__1¶¶¶11____11¶¶8___1¶_¶8_1__11¶_____________");
+            Console.WriteLine("__¶¶¶¶¶¶88888¶¶¶¶¶888¶¶¶¶¶¶¶¶¶¶¶1_8¶1111_____");
+            Console.WriteLine("_18____________________________88__1_1_______");
+            Console.WriteLine("_18____________________________18___8¶11818¶_");
+            Console.WriteLine("_18____________________________18___81118888_");
+
+        }
+    }
+}
